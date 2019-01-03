613104da755a2864ec62425cd70ff14e610d2d9d
Chibi1 <chibueze.e.njoku@gmail.com>
2019-01-03 13:18:52 +1100

    stringsAndImages

---
+++ coreProgramming/howToCode_simpleData/week1/stringsAndImages.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname stringsAndImages) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+"apple"
+"Ada"
+
+(string-append "Ada" " " "Lovelace")
+
+(string-length "apple")
+
+(substring "Caribou" 2 4)
+
+(substring "0123456789" 2 4)
