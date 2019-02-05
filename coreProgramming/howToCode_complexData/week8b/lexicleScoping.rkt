d45cbce5e0e4665a392cc73e64128374d6cad250
Chibi1 <chibueze.e.njoku@gmail.com>
2019-02-05 17:07:10 +1100

    lexicleScoping

---
+++ coreProgramming/howToCode_complexData/week8b/lexicleScoping.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname lexicleScoping) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+
+(define p "incendio")
+
+
+(local [(define p "accio ")
+        (define (fetch n) (string-append p n))]
+
+  (fetch "portkey"))
+
+(define a 1)
+(define b 2)
+
+(+ a
+   (local [(define b 3)]
+     (+ a b))
+   b)
